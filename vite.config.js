8c80976cb64a398609b38521c57813c4bc5584d5
piyushxyz1 <piyushnegi033@gmail.com>
2024-09-09 22:49:46 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/game-app.git',
+  base:'/game-app',
   plugins: [react()],
   
 })
